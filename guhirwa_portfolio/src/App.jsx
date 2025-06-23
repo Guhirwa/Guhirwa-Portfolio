74cb975aea0f7eb3924bc44fa330178f6c58ca2f
Guhirwa <guhirwachristian004@gmail.com>
2025-06-23 15:50:49 +0200

    Working on Landing page part 1

--- guhirwa_portfolio/src/App.jsx
+++ guhirwa_portfolio/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import HeroGradient from './assets/components/heroSection/HeroGradient'
+import HeroMain from './assets/components/heroSection/HeroMain'
 import NavbarMain from './assets/components/navbar/NavbarMain'
 
 function App() {
   return (
     <main className='font-body'>
       <NavbarMain />
+      <HeroMain />
+      <HeroGradient />
     </main>
   )
 }
---
+++ guhirwa_portfolio/src/assets/components/heroSection/HeroGradient.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HeroGradient() {
+  return (
+    <div>
+      <div className='shadow-cyanMediumShadow absolute top-0 right-[400px] -z-10 animate-pulse'></div>
+      <div className='shadow-orangeMediumShadow absolute top-0 right-0 -z-10 animate-pulse'></div>
+    </div>
+  )
+}
+
+export default HeroGradient
