e58f0b31febc8846c2d8c0202f0d3a51f7751576
Guhirwa <guhirwachristian004@gmail.com>
2025-06-23 08:42:09 +0200

    Install React Icons and Refine project Structure

---
+++ guhirwa_portfolio/src/assets/components/navbar/NavbarLogo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavbarLogo() {
+  return (
+    <div>
+        <h1 className='text-white text-2xl'>Christian GUHIRWA</h1>
+    </div>
+  )
+}
+
+export default NavbarLogo
