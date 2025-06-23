64146a3a8aa1681e4a9527e1544120dc7c6dd753
Guhirwa <guhirwachristian004@gmail.com>
2025-06-23 13:47:05 +0200

    NavBar Designing - I worked on Navbar to have all required general links and needs. - Insuring the responciveness of that navbar in different devices with difference screen sizes. - Application of some JavaScript hand in hand with tailwind to ensure smooth toggle of my menu and links

--- guhirwa_portfolio/src/assets/components/navbar/NavbarLinks.jsx
+++ guhirwa_portfolio/src/assets/components/navbar/NavbarLinks.jsx
@@ -9,7 +9,7 @@ const links = [
 ];
 const NavbarLinks = () => {
   return (
-    <ul className='flex gap-6 text-white font-bold text-center py-4'>
+    <ul className='flex gap-6 text-white font-bold text-center py-4 lg:flex-row sm:flex-col lg:relative sm:absolute sm:top-[120%] left-[50%] -translate-x-[50%] lg:text-xl sm:text-xl sm:bg-cyan/30 backdrop-blur-lg lg:bg-black sm:w-full'>
         {links.map((link, index) => {
             return <li key = {index} className='group'>
                 <a href='#' className='cursor-pointer text-white hover:text-cyan transition-all duration-500'>{link.link}</a>
