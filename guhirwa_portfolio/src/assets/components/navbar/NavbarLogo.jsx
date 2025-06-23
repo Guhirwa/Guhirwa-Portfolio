626c9098f9f47aac5d3c99b55517c749c86b4b9f
Guhirwa <guhirwachristian004@gmail.com>
2025-06-23 14:23:34 +0200

    Push navbar's Logo component

--- guhirwa_portfolio/src/assets/components/navbar/NavbarLogo.jsx
+++ guhirwa_portfolio/src/assets/components/navbar/NavbarLogo.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 function NavbarLogo() {
   return (
     <div>
-        <h1 className='text-white text-2xl'>Christian GUHIRWA</h1>
+        <h1 className='text-white text-2xl sm:hidden md:block'>Christian GUHIRWA</h1>
+        <h1 className='text-white font-special font-extrabold text-4xl sm:block md:hidden'>CG</h1>
     </div>
   )
 }
