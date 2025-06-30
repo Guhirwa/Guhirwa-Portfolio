054cf93989d75ea350f221e4dec70aca51041168
Guhirwa <guhirwachristian004@gmail.com>
2025-06-30 18:13:20 +0200

    Fixing image error

--- guhirwa_portfolio/src/assets/components/aboutMeSection/AboutMeImage.jsx
+++ guhirwa_portfolio/src/assets/components/aboutMeSection/AboutMeImage.jsx
@@ -4,7 +4,11 @@ const AboutMeImage = () => {
   return (
     <div className='h-[500px] w-[300px] relative'>
         <div className='h-[500px] w-[300px] rounded-[100px] absolute overflow-hidden'>
-            <img className='h-4 w-auto object-cover' src="../../../../public/images/About_Me_Image.png" alt="About me image" />
+            <img 
+                src="../../../../public/images/About_Me_Image.png" 
+                alt="About me image" 
+                className='h-4 w-auto object-cover'
+            />
         </div>
         <div>
 
