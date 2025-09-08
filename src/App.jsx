8b315f45bad6e1a0264ef5596d35165560fdbe26
Guhirwa <christianguhirwa74@gmail.com>
2025-09-08 21:52:17 +0200

    Working on the Core skills at the bottom of Hero. - There are three core skills on the larger screen. - They tend to change the font size as the responce of the changing in screen size. - At the screen size of (sm) the only one skill remain.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import NavbarMain from './components/navbar/NavbarMain'
 import HeroMain from './components/heroSection/HeroMain'
 import HeroGradient from './components/heroSection/HeroGradient'
+import SubHeroSection from './components/heroSection/SubHeroSection'
 
 const App = () => {
   return (
     <main className='font-body'>
       <NavbarMain />
       <HeroMain />
+      <SubHeroSection />
     </main>
   )
 }
--- src/components/heroSection/HeroGradient.jsx
+++ src/components/heroSection/HeroGradient.jsx
@@ -4,6 +4,9 @@ const HeroGradient = () => {
   return (
     <div>
       <div className='shadow-cyan-medium absolute top-0 right-[400px] -z-10 animated-pulse'></div>
+      <div className='shadow-orange-medium absolute top-0 right-[0px] -z-10 animated-pulse'></div>
+      <div className='shadow-cyan-medium absolute top-[300px] left-0 -z-10 animated-pulse opacity-50'></div>
+      <div className='shadow-orange-medium absolute top-[500px] left-0 -z-10 animated-pulse opacity-50'></div>
     </div>
   )
 }
