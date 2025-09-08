644efb8f00b3b66922321c7e99e2b196dd933b8f
Guhirwa <christianguhirwa74@gmail.com>
2025-09-08 16:34:43 +0200

    Working on Hero heroSection - Welcome Text, names and career - Image and the rounded design

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { PiCircleDashedThin } from 'react-icons/pi'
+import profileImage from '../../../public/images/profileImg.jpeg'
 
 const HeroPic = () => {
   return (
-    <div>HeroPic</div>
+    <div className='h-full flex items-center justify-center'>
+        <img src={profileImage} alt="Christian's profile image" className='max-h-[250px] w-auto rounded-full' />
+        <div className='absolute -z-10 flex justify-center items-center animate-pulse'>
+          <PiCircleDashedThin className='md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite' />
+        </div>
+    </div>
   )
 }
 
--- src/components/heroSection/HeroText.jsx
+++ src/components/heroSection/HeroText.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const HeroText = () => {
   return (
-    <div>
-        <h2>FRONT-END WEB DEVELOPMENT</h2>
-        <h1>CHRISTIAN GUHIRWA</h1>
-        <p></p>
+    <div className='flex flex-col gap-4 h-full justify-center md:text-left sm:text-center'>
+        <h2 className='lg:text-2xl sm:text-xl uppercase text-cyan'>FRONT-END WEB DEVELOPMENT</h2>
+        <h1 className='md:text-[2.8rem] lg:text-6xl sm:text-4xl font-bold font-special text-orange'>CHRISTIAN GUHIRWA</h1>
+        <p className='text-lg mt-4 text-white'>A passionate Web Developer, IT Training facilitator and <br />  Software Engineer with 1+ years of experience.</p>
     </div>
   )
 }
