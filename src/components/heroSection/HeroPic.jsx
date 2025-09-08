dd50a17c63afc558066ec1d361dfe7ae28318c13
Guhirwa <christianguhirwa74@gmail.com>
2025-09-08 17:08:37 +0200

    Refining image and shape position to fit the different screen sizes

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -4,10 +4,10 @@ import profileImage from '../../../public/images/profileImg.jpeg'
 
 const HeroPic = () => {
   return (
-    <div className='h-full flex items-center justify-center'>
+    <div className='h-full flex items-center justify-center mt-16'>
         <img src={profileImage} alt="Christian's profile image" className='max-h-[250px] w-auto rounded-full' />
         <div className='absolute -z-10 flex justify-center items-center animate-pulse'>
-          <PiCircleDashedThin className='md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite' />
+          <PiCircleDashedThin className='md:h-[20%] sm:h-[30%] min-h-[400px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite' />
         </div>
     </div>
   )
