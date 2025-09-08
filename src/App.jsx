39a6da10587c03166f7b52838b842210e5907459
Guhirwa <christianguhirwa74@gmail.com>
2025-09-08 21:31:08 +0200

    Working on Hero HeroGradient - I created the div for different color shadow animation - This component is not finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavbarMain from './components/navbar/NavbarMain'
 import HeroMain from './components/heroSection/HeroMain'
+import HeroGradient from './components/heroSection/HeroGradient'
 
 const App = () => {
   return (
--- src/components/heroSection/HeroGradient.jsx
+++ src/components/heroSection/HeroGradient.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const HeroGradient = () => {
   return (
-    <div>HeroGradient</div>
+    <div>
+      <div className='shadow-cyan-medium absolute top-0 right-[400px] -z-10 animated-pulse'></div>
+    </div>
   )
 }
 
