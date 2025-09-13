f4626a9d20ebe35ee5819abb33380c60251dd710
Guhirwa <christianguhirwa74@gmail.com>
2025-09-14 00:52:22 +0200

    Working on Projects section but not yet finished

---
+++ src/components/projectsSection/ProjectText.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ProjectText = () => {
+  return (
+    <div className='flex flex-col items-center mt-[100px]'>
+        <h2 className='text-6xl text-cyan mb-10'>Projects</h2>
+        <p className='text-lg text-center text-white'>
+            I have worked on a Variety of Web development projects ranging from responsive
+            websites for small and simple bussiness to full stack applications and complex front
+            end interfaces.
+        </p>
+    </div>
+  )
+}
+
+export default ProjectText
